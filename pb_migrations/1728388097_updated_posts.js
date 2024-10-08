9e74447ecaba6c8efbe11fa624cf8472d02688da
pizzaguyapprentice <matisew8247@gmail.com>
2024-10-08 15:33:03 +0100

    second commit not as awesome

---
+++ pb_migrations/1728388097_updated_posts.js
@@ -0,0 +1,62 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("om8a65z2op0pv4y")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fo7wqnlo",
+    "name": "datecreated",
+    "type": "date",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": "",
+      "max": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fsssr7q4",
+    "name": "content",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("om8a65z2op0pv4y")
+
+  // remove
+  collection.schema.removeField("fo7wqnlo")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fsssr7q4",
+    "name": "field",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
---
+++ pb_migrations/1728391311_updated_note.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("om8a65z2op0pv4y")
+
+  collection.name = "notes"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("om8a65z2op0pv4y")
+
+  collection.name = "note"
+
+  return dao.saveCollection(collection)
+})
